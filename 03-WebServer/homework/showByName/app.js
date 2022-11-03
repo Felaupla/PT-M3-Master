bc3371327e600e08c0acf43ab01060cae46d5ee1
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2022-11-03 18:41:23 -0300

    Homework 03 WebServer

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,18 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs");
+var http = require("http");
 
 // Escribí acá tu servidor
+http
+  .createServer(function (req, res) {
+    console.log(`Llegò un request: ${req.url}`);
+    fs.readFile(`./images${req.url}.jpg`, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "content-type": "text/plain" });
+        http.request.end("Img not found");
+      } else {
+        res.writeHead(200, { "content-type": "image/jpeg" });
+        res.end(data);
+      }
+    });
+  })
+  .listen(1337, "127.0.0.1");
